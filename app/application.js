30ebdde3078f467c7e641d4082d58df2ba48aa15
jacobkim9881 <jacobkim9881@gmail.com>
2022-06-08 10:41:20 +0900

    chore: deleted unwanted css for ios

--- app/application.js
+++ app/application.js
@@ -3,9 +3,6 @@ import React from 'react';
 import Router from 'react-router';
 import Routes from './config/Routes';
 import ApplicationStore from './stores/ApplicationStore';
-import '../app/assets/scss/style.scss';
-import '../bower_components/ratchet/dist/css/ratchet.min.css';
-import '../bower_components/ratchet/dist/css/ratchet-theme-ios.min.css';
 
 Router.run(Routes, Router.HistoryLocation, function (Handler) {
   React.render(<Handler />, document.body);
